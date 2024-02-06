899cb3987acbcf7eb42e7bf3868759385f98f1f6
Harmanpreet2002 <Harmanpreet.stakkdev@gmail.com>
2024-02-06 17:52:13 +0530

    Added env and SidebarContent changes

--- .code/src/components/VerticalLayout/SidebarContent.js
+++ .code/src/components/VerticalLayout/SidebarContent.js
@@ -704,22 +704,23 @@ const SidebarContent = (props) => {
                       </li>   */}
                   </ul>
                 </li>
-                <li>
-                  <Link to="/#" className="has-arrow ">
-                    <FeatherIcon icon="file-text" />
-                    <span>Manager Reports</span>
-                  </Link>
-                  <ul className="sub-menu">
-                    <li>
-                      <Link to="/work-scheduling">Work Schedule</Link>
-                    </li>
-                  {decode.company == "GSK Properties" && (
+                {decode.company == "GSK Properties" && role === "company" && (
+                  <li>
+                    <Link to="/#" className="has-arrow ">
+                      <FeatherIcon icon="file-text" />
+                      <span>Manager Reports</span>
+                    </Link>
+                    <ul className="sub-menu">
+                      <li>
+                        <Link to="/work-scheduling">Work Schedule</Link>
+                      </li>
+
                       <li>
                         <Link to="/calendar-reports">Management Reports</Link>
                       </li>
-                    )}  
-                  </ul>
-                </li>
+                    </ul>
+                  </li>
+                )}
 
                 <li className="facilities-li">
                   <Link to="/widget">
